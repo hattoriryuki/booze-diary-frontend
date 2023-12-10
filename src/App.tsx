cc9ea0e824d73ba682ce61baefb2b1258d6da06a
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-11 00:36:38 +0900

    improve: header、footerをコンポーネント化

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,14 @@
-import {
-  Box,
-  ChakraProvider,
-  Flex,
-  Image,
-  Link,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "./theme/theme";
-import logoImage from "./assets/images/logo.png";
+import { Header } from "./components/organisms/layout/Header";
+import { Footer } from "./components/organisms/layout/Footer";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Flex as={"header"} h="64px" justify="space-between" alignItems="center">
-        <Image
-          src={logoImage}
-          alt="Logo image"
-          boxShadow="none"
-          cursor="pointer"
-        />
-        <Box>
-          <Link mr={4}>ログイン</Link>
-          <Link mr={4}>新規登録</Link>
-        </Box>
-      </Flex>
-      <Flex
-        as={"footer"}
-        h="56px"
-        position="fixed"
-        bottom={0}
-        left="50%"
-        transform="translate(-50%, 0)"
-        direction="column"
-        alignItems="center"
-        justify="center"
-      >
-        <Stack direction="row" gap={12}>
-          <Link>利用規約</Link>
-          <Link>プライバシーポリシー</Link>
-          <Link>お問い合せ</Link>
-        </Stack>
-        <Text color="gray.600">
-          &copy; 2023 BoozeDiary. All rights reserved
-        </Text>
-      </Flex>
+      <Header />
+      <Footer />
     </ChakraProvider>
   );
 }
