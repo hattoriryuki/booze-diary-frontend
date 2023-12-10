4ec57d215d6a911e12be4e24764bee34442a6108
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-11 00:12:05 +0900

    feat: ヘッダーの雛形を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, Flex, Image, Link } from "@chakra-ui/react";
 
 import { theme } from "./theme/theme";
+import logoImage from "./assets/images/logo.png";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button>ボタン</Button>
+      <Flex as={"header"} h="64px" justify="space-between" alignItems="center">
+        <Image src={logoImage} alt="Logo image" boxShadow="none" cursor="pointer" />
+        <Box>
+          <Link mr={4}>ログイン</Link>
+          <Link mr={4}>新規登録</Link>
+        </Box>
+      </Flex>
     </ChakraProvider>
   );
 }
