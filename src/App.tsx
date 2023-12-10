f0cff1459354fbf0678cebfa6e2f8d8928069ee8
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-11 00:28:47 +0900

    feat: フッターの雛形を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { Box, ChakraProvider, Flex, Image, Link } from "@chakra-ui/react";
+import {
+  Box,
+  ChakraProvider,
+  Flex,
+  Image,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 import { theme } from "./theme/theme";
 import logoImage from "./assets/images/logo.png";
@@ -7,11 +15,36 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Flex as={"header"} h="64px" justify="space-between" alignItems="center">
-        <Image src={logoImage} alt="Logo image" boxShadow="none" cursor="pointer" />
+        <Image
+          src={logoImage}
+          alt="Logo image"
+          boxShadow="none"
+          cursor="pointer"
+        />
         <Box>
           <Link mr={4}>ログイン</Link>
           <Link mr={4}>新規登録</Link>
         </Box>
+      </Flex>
+      <Flex
+        as={"footer"}
+        h="56px"
+        position="fixed"
+        bottom={0}
+        left="50%"
+        transform="translate(-50%, 0)"
+        direction="column"
+        alignItems="center"
+        justify="center"
+      >
+        <Stack direction="row" gap={12}>
+          <Link>利用規約</Link>
+          <Link>プライバシーポリシー</Link>
+          <Link>お問い合せ</Link>
+        </Stack>
+        <Text color="gray.600">
+          &copy; 2023 BoozeDiary. All rights reserved
+        </Text>
       </Flex>
     </ChakraProvider>
   );
