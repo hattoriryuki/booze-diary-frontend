a4640d5d72a736481d333b6850bdb3cca8f73393
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-12 04:37:53 +0900

    add: トップ画像を追加

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,5 +1,12 @@
 import { FC, memo } from "react";
+import { Box, Image } from "@chakra-ui/react";
+
+import topImage from "../../assets/images/top.jpg";
 
 export const Top: FC = memo(() => {
-  return <div>トップページです</div>;
+  return (
+    <Box w="100vw">
+      <Image src={topImage} alt="Top image" w="100%" />
+    </Box>
+  );
 });
