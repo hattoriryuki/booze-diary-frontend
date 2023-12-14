a4207b7a45786b5a8ed9e49140de6b5502f3fcc5
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-15 06:25:25 +0900

    feat: トップとログインページのルーティングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { theme } from "./theme/theme";
 import { DefaultLayout } from "./components/templates/DefaultLayout";
 import { Top } from "./components/pages/Top";
+import { Login } from "./components/pages/Login";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <DefaultLayout>
-        <Top />
-      </DefaultLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <DefaultLayout>
+                <Top />
+              </DefaultLayout>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <DefaultLayout>
+                <Login />
+              </DefaultLayout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
