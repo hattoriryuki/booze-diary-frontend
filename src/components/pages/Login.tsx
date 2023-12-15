41bb777da4588591e58f6e707b926d8b44761284
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-15 22:10:33 +0900

    feat: Loginページの雛形を追加

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,48 @@
-import { FC, memo } from "react";
+import { FC, memo, useCallback } from "react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Input,
+  InputProps,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+
+import { PrimaryButton } from "../atoms/PrimaryButton";
 
 export const Login: FC = memo(() => {
-  return <div>Loginページです</div>;
+  const StyledInput: FC<InputProps> = useCallback(({ ...props }) => {
+    return <Input shadow="md" bg="#F3F3F3" border="none" {...props} />;
+  }, []);
+
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      justify="space-around"
+      mt={16}
+      w="100%"
+      h="calc(100vh - 120px)"
+    >
+      <Heading color="#0a2463" mt={{ base: "12", md: "8" }}>
+        ログイン
+      </Heading>
+      <Flex direction="column" align="center" w="100%">
+        <Box w={{ base: "90%", md: "40%" }}>
+          <Text>メールアドレス</Text>
+          <StyledInput type="email" />
+        </Box>
+        <Box w={{ base: "90%", md: "40%" }} mt={10}>
+          <Text>パスワード</Text>
+          <StyledInput type="password" />
+        </Box>
+      </Flex>
+      <PrimaryButton>ログイン</PrimaryButton>
+      <Box>
+        <Link mr={8}>新規登録</Link>
+        <Link>パスワードをお忘れの方</Link>
+      </Box>
+    </Flex>
+  );
 });
