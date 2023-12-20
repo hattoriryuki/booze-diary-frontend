8f09642a38e0ebab244148f19d00d7da78f28b8d
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-20 22:23:17 +0900

    improve: インプットエリアをコンポーネント化

---
+++ src/components/molecules/PrimaryInputArea.tsx
@@ -0,0 +1,24 @@
+import { ChangeEvent, FC, memo } from "react";
+import { Box, BoxProps, Text } from "@chakra-ui/layout";
+
+import { PrimaryInput } from "../atoms/PrimaryInput";
+
+type Params = {
+  title: string;
+  type: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+};
+
+export const PrimaryInputArea: FC<{ params: Params } & BoxProps> = memo(
+  ({ params, ...props }) => {
+    const { title, type, onChange, value } = params;
+
+    return (
+      <Box w={{ base: "90%", md: "50%" }} {...props}>
+        <Text>{title}</Text>
+        <PrimaryInput type={type} argument={{ onChange, value }} />
+      </Box>
+    );
+  }
+);
