687cf009663e1a9c5383c23c47e4f63641b2a895
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-25 23:59:25 +0900

    feat: 星ボタンのイベントを追加

--- src/components/molecules/StarButtons.tsx
+++ src/components/molecules/StarButtons.tsx
@@ -1,31 +1,40 @@
-import { FC, memo } from "react";
-import { Button, ButtonProps } from "@chakra-ui/button";
-import { Flex } from "@chakra-ui/layout";
+import { FC, memo, useCallback, useState } from "react";
+import { Button } from "@chakra-ui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as faStarSub } from "@fortawesome/free-solid-svg-icons";
 
 export const StarButtons: FC = memo(() => {
-  const StarButton = ({ ...props }: ButtonProps) => {
-    return (
-      <Button
-        bg="none"
-        px={0}
-        minW="none"
-        fontSize="x-large"
-        _hover={{ border: "1px", borderColor: "white" }}
-        {...props}
-      >
-        <FontAwesomeIcon icon={faStar} />
-      </Button>
-    );
-  };
+  const [ratings, setRatings] = useState([false, false, false, false, false]);
+
+  const onClickSubmit = useCallback((index: number) => {
+    const submitted = [...ratings];
+    submitted[index] = !ratings[index];
+    setRatings(submitted);
+  }, [ratings]);
+
   return (
-    <Flex>
-      <StarButton />
-      <StarButton />
-      <StarButton />
-      <StarButton />
-      <StarButton />
-    </Flex>
+    <>
+      {ratings.map((val, index) => {
+        return (
+          <Button
+            key={index}
+            bg="none"
+            px={0}
+            minW="none"
+            fontSize="x-large"
+            _hover={{ cursor: "pointer" }}
+            _active={{ bg: "none" }}
+            onClick={() => onClickSubmit(index)}
+          >
+            {val ? (
+              <FontAwesomeIcon icon={faStarSub} />
+            ) : (
+              <FontAwesomeIcon icon={faStar} />
+            )}
+          </Button>
+        );
+      })}
+    </>
   );
 });
