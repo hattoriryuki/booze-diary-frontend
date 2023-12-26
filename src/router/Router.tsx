abe4e86a7d3ccdf245ca9ba9700bce760fc1894b
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-12-26 13:00:55 +0900

    improve: Router.tsx内を整理

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,57 +2,20 @@ import { FC, memo } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { DefaultLayout } from "../components/templates/DefaultLayout";
-import { Login } from "../components/pages/Login";
-import { Top } from "../components/pages/Top";
 import { LoginUserProvider } from "../providers/LoginUserProvider";
-import { SignUp } from "../components/pages/SignUp";
-import { Index } from "../components/pages/Index";
-import { PostCreation } from "../components/pages/PostCreation";
+import { homeRoutes } from "./HomeRoutes";
 
 export const Router: FC = memo(() => {
   return (
     <LoginUserProvider>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <DefaultLayout>
-              <Top />
-            </DefaultLayout>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <DefaultLayout>
-              <Login />
-            </DefaultLayout>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <DefaultLayout>
-              <SignUp />
-            </DefaultLayout>
-          }
-        />
-        <Route
-          path="/index"
-          element={
-            <DefaultLayout>
-              <Index />
-            </DefaultLayout>
-          }
-        />
-        <Route
-          path="/new"
-          element={
-            <DefaultLayout>
-              <PostCreation />
-            </DefaultLayout>
-          }
-        />
+        {homeRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={`${route.path}`}
+            element={<DefaultLayout>{route.element}</DefaultLayout>}
+          />
+        ))}
       </Routes>
     </LoginUserProvider>
   );
