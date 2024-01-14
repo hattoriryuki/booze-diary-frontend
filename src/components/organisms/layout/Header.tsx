11323c59873684a44292b33754d81b2cc2368de0
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-01-14 23:51:51 +0900

    feat: サインアウト機能を追加

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,17 +1,36 @@
 import { FC, memo, useCallback, useContext } from "react";
 import { Box, Flex, Image, Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 import logoImage from "../../../assets/images/logo.png";
 import { LoginUserContext } from "../../../providers/LoginUserProvider";
+import { signOutReq } from "../../../api/auth";
+import { useToastMsg } from "../../../hooks/useToastMsg";
 
 export const Header: FC = memo(() => {
   const navigate = useNavigate();
-  const { isSignedIn } = useContext(LoginUserContext);
+  const { isSignedIn, setIsSignedIn, setCurrentUser } =
+    useContext(LoginUserContext);
+  const { showToastMsg } = useToastMsg();
 
   const onClickLogin = useCallback(() => navigate("/login"), []);
   const onClickSignUp = useCallback(() => navigate("/signup"), []);
   const onClickTop = useCallback(() => navigate("/"), []);
+  const onClickSignOut = async () => {
+    try {
+      const res = await signOutReq();
+      Cookies.remove("_access_token");
+      Cookies.remove("_client");
+      Cookies.remove("_uid");
+      setIsSignedIn(false);
+      setCurrentUser(undefined);
+      navigate("/posts");
+      showToastMsg({ status: "info", title: "ログアウトしました" });
+    } catch (err) {
+      showToastMsg({ status: "error", title: "ログアウトに失敗しました" });
+    }
+  };
 
   return (
     <Flex
@@ -34,7 +53,9 @@ export const Header: FC = memo(() => {
       />
       <Box>
         {isSignedIn ? (
-          <Link mr={4}>ログアウト</Link>
+          <Link mr={4} onClick={onClickSignOut}>
+            ログアウト
+          </Link>
         ) : (
           <>
             <Link mr={4} onClick={onClickLogin}>
