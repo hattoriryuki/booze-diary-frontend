f95c1672791e143a72e321a1fc0ef6a0b46bce75
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-01-15 00:00:04 +0900

    improve: サインアウトをカスタムフック化

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,36 +1,20 @@
 import { FC, memo, useCallback, useContext } from "react";
 import { Box, Flex, Image, Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
 
 import logoImage from "../../../assets/images/logo.png";
 import { LoginUserContext } from "../../../providers/LoginUserProvider";
-import { signOutReq } from "../../../api/auth";
-import { useToastMsg } from "../../../hooks/useToastMsg";
+import { useSignOut } from "../../../hooks/useSignOut";
 
 export const Header: FC = memo(() => {
   const navigate = useNavigate();
-  const { isSignedIn, setIsSignedIn, setCurrentUser } =
-    useContext(LoginUserContext);
-  const { showToastMsg } = useToastMsg();
+  const { isSignedIn } = useContext(LoginUserContext);
+  const { signOut } = useSignOut();
 
   const onClickLogin = useCallback(() => navigate("/login"), []);
   const onClickSignUp = useCallback(() => navigate("/signup"), []);
   const onClickTop = useCallback(() => navigate("/"), []);
-  const onClickSignOut = async () => {
-    try {
-      const res = await signOutReq();
-      Cookies.remove("_access_token");
-      Cookies.remove("_client");
-      Cookies.remove("_uid");
-      setIsSignedIn(false);
-      setCurrentUser(undefined);
-      navigate("/posts");
-      showToastMsg({ status: "info", title: "ログアウトしました" });
-    } catch (err) {
-      showToastMsg({ status: "error", title: "ログアウトに失敗しました" });
-    }
-  };
+  const onClickSignOut = useCallback(() => signOut(), []);
 
   return (
     <Flex
---
+++ src/hooks/useSignOut.ts
@@ -0,0 +1,29 @@
+import { useCallback, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+
+import { signOutReq } from "../api/auth";
+import { LoginUserContext } from "../providers/LoginUserProvider";
+import { useToastMsg } from "./useToastMsg";
+
+export const useSignOut = () => {
+  const { setIsSignedIn, setCurrentUser } = useContext(LoginUserContext);
+  const { showToastMsg } = useToastMsg();
+  const navigate = useNavigate();
+
+  const signOut = useCallback(async () => {
+    try {
+      const res = await signOutReq();
+      Cookies.remove("_access_token");
+      Cookies.remove("_client");
+      Cookies.remove("_uid");
+      setIsSignedIn(false);
+      setCurrentUser(undefined);
+      navigate("/posts");
+      showToastMsg({ status: "info", title: "ログアウトしました" });
+    } catch (err) {
+      showToastMsg({ status: "error", title: "ログアウトに失敗しました" });
+    }
+  }, []);
+  return { signOut };
+};
