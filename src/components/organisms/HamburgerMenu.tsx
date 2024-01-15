d378ce83cc5301570fb6093b1dd59a15dd7e4317
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-01-15 23:32:42 +0900

    feat: ハンバーガーメニューの雛形を追加

--- src/components/organisms/HamburgerMenu.tsx
+++ src/components/organisms/HamburgerMenu.tsx
@@ -1,39 +1,48 @@
-import { FC, memo } from "react";
+import { FC, memo, useCallback, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faAddressCard } from "@fortawesome/free-regular-svg-icons";
 import {
+  Avatar,
   Menu,
   MenuButton,
   MenuDivider,
   MenuGroup,
   MenuItem,
   MenuList,
+  Text,
 } from "@chakra-ui/react";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { LoginUserContext } from "../../providers/LoginUserProvider";
+import { useSignOut } from "../../hooks/useSignOut";
 
 export const HamburgerMenu: FC = memo(() => {
+  const { currentUser } = useContext(LoginUserContext);
+  const { signOut } = useSignOut();
+
+  const onClickSignOut = useCallback(() => signOut(), []);
+
   return (
     <Menu autoSelect={false}>
-      <MenuButton>
-        <FontAwesomeIcon icon={faBars} />
+      <MenuButton mr={8}>
+        <Avatar src={currentUser?.image} />
       </MenuButton>
-      <MenuList>
-        <MenuGroup color="#0a2463" title="Action">
-          <MenuItem>投稿する</MenuItem>
-        </MenuGroup>
-        <MenuDivider />
-        <MenuGroup color="#0a2463" title="Link">
-          <MenuItem>マイページ</MenuItem>
-          <MenuItem>トップ</MenuItem>
-          <MenuItem>ログアウト</MenuItem>
-        </MenuGroup>
-        <MenuDivider />
-        <MenuGroup color="#0a2463" title="Creator">
-          <MenuItem>X</MenuItem>
-          <MenuItem>GitHub</MenuItem>
-        </MenuGroup>
-        <MenuDivider />
-        <MenuGroup color="#0a2463" title="Help">
-          <MenuItem>お問い合わせ</MenuItem>
+      <MenuList minW="12rem">
+        <MenuGroup
+          color="#0a2463"
+          title={currentUser?.name}
+          fontSize="xl"
+          mt={1}
+        >
+          <MenuDivider />
+          <MenuItem>
+            <FontAwesomeIcon icon={faAddressCard} />
+            <Text ml={2}>マイページ</Text>
+          </MenuItem>
+          <MenuItem color="red.500" onClick={onClickSignOut}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <Text ml={2}>ログアウト</Text>
+          </MenuItem>
         </MenuGroup>
       </MenuList>
     </Menu>
