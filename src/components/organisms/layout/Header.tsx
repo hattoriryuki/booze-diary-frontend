dd56cd296b1f0acf2e584f5c4143ef103a935e14
hattori ryuki <ryuki.97913795@i.softbank.jp>
2024-01-16 23:35:28 +0900

    feat: ヘッダーのリンクを動的に変える機能を追加

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,6 +1,14 @@
-import { FC, memo, useCallback, useContext } from "react";
+import {
+  FC,
+  ReactNode,
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { Box, Flex, Image, Text, Link as ChakraLink } from "@chakra-ui/react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 
@@ -9,12 +17,26 @@ import { LoginUserContext } from "../../../providers/LoginUserProvider";
 import { HamburgerMenu } from "../HamburgerMenu";
 
 export const Header: FC = memo(() => {
+  const [link, setLink] = useState<ReactNode>();
   const navigate = useNavigate();
   const { isSignedIn } = useContext(LoginUserContext);
+  const location = useLocation();
+
+  const selectLink = () => {
+    if (location.pathname === "/posts") {
+      setLink(<Link to="/">トップ</Link>);
+    } else {
+      setLink(<Link to="/posts">ギャラリー</Link>);
+    }
+  };
 
   const onClickLogin = useCallback(() => navigate("/login"), []);
   const onClickSignUp = useCallback(() => navigate("/signup"), []);
   const onClickTop = useCallback(() => navigate("/"), []);
+
+  useEffect(() => {
+    selectLink();
+  }, [location]);
 
   return (
     <Flex
@@ -38,12 +60,7 @@ export const Header: FC = memo(() => {
       <Box>
         {isSignedIn ? (
           <Flex align="center" gap={7}>
-            <ChakraLink>
-              <Link to="/">トップ</Link>
-            </ChakraLink>
-            <ChakraLink>
-              <Link to="/posts">ギャラリー</Link>
-            </ChakraLink>
+            <ChakraLink>{link}</ChakraLink>
             <ChakraLink>
               <Link to="/posts/new">
                 <Flex align="center">
